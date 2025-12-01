730d310df3df1bc9afd463e6cd6d79662fab6911
Dr Anjit <taanjit@gmail.com>
2025-12-01 09:05:06 +0000

    Add MLflow experiment tracking with Wine classification model

--- src/file1.py
+++ src/file1.py
@@ -14,12 +14,13 @@ wine = load_wine()
 X = wine.data
 y = wine.target
 
+
 # Train test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
 
 # Define the params for RF model
 max_depth = 10
-n_estimators = 5
+n_estimators = 100
 
 # Mention your experiment below
 mlflow.set_experiment('YT-MLOPS-Exp1')
@@ -51,7 +52,7 @@ with mlflow.start_run():
     mlflow.log_artifact(__file__)
 
     # tags
-    mlflow.set_tags({"Author": 'Vikash', "Project": "Wine Classification"})
+    mlflow.set_tags({"Author": 'Anjit', "Project": "Wine Classification"})
 
     # Log the model
     mlflow.sklearn.log_model(rf, "Random-Forest-Model")
